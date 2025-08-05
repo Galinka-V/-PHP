3cc7af5c7243163b1223d967e1192e20dbca8377
Galinka-V <gavi68158@gmail.com>
2025-08-05 13:17:31 +0300

    Update index3.php
    Осталось реализовать использование функций библиотеки mbstring для поддержки кириллицы, а также корректировать регистр данных, как указано в задании.

--- index3.php
+++ index3.php
@@ -1,5 +1,4 @@
 <?php 
-// Запросить три переменные с именем, фамилией и отчеством.
 echo "Введите имя: ";
 $firstName = trim(fgets(STDIN));
 
@@ -9,18 +8,22 @@ $middleName = trim(fgets(STDIN));
 echo "Введите фамилию: ";
 $lastName = trim(fgets(STDIN));
 
-//Объявить переменную $fullname, записать в неё полное ФИО так, чтобы каждое слово начиналось с большой буквы.  
-$fullname = "$lastName $firstName $middleName";
-echo "Полное имя:". ucwords($fullname), PHP_EOL; 
 
-//Объявить переменную $surnameAndInitials. Её значением должна быть конкатенация фамилии с первой буквой в верхнем регистре, а также через пробел — инициалов. После каждой буквы инициалов должна быть точка.
+function mb_ucfirst($string) {
+    return mb_strtoupper(mb_substr($string, 0, 1)) . mb_substr($string, 1);
+}
 
-$I=substr(ucfirst($firstName), 0, 1);
-$O=substr(ucfirst($middleName), 0, 1);
-$surnameAndInitials = ucfirst($lastName)." ". $I.".".$O.".";
-echo "Фамилия и инициалы:".$surnameAndInitials, PHP_EOL;
 
-//Объявить переменную $fio. Значением должна быть конкатенация первых букв в верхнем регистре.
-$F=substr(ucfirst($lastName), 0, 1);
-echo "Аббревиатура:".$F.$I.$O, PHP_EOL;
+$fullname = mb_ucfirst($lastName) . ' ' . mb_ucfirst($firstName) . ' ' . mb_ucfirst($middleName);
+echo "Полное имя: " . $fullname . PHP_EOL; 
+
+
+$I = mb_strtoupper(mb_substr($firstName, 0, 1));
+$O = mb_strtoupper(mb_substr($middleName, 0, 1));
+$surnameAndInitials = mb_ucfirst($lastName) . " " . $I . "." . $O . ".";
+echo "Фамилия и инициалы: " . $surnameAndInitials . PHP_EOL;
+
+
+$F = mb_strtoupper(mb_substr($lastName, 0, 1));
+echo "Аббревиатура: " . $F . $I . $O . PHP_EOL;
 ?>
