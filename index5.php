c8a17fcdac4df5b6d6b5c38366b26dab7c2911b2
Galinka-V <gavi68158@gmail.com>
2025-08-14 23:01:01 +0300

    Create index5.php
    Домашнее задание к занятию "Основы работы с объектами"

---
+++ index5.php
@@ -0,0 +1,51 @@
+<?php
+
+function generateList($year, $month) {
+    // определяю имя месяца
+    $monthName = date("F", mktime(0, 0, 0, $month, 1, $year));
+    
+    // количество дней в месяце
+    $countDays = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+    
+    // cписок всех дней месяца
+    $list = [];
+    
+    // с 1-го числа
+    $currentDay = 1;
+    
+    // pабочие дни (сутки через двое)
+    $workDays = 0;
+    
+    while ($currentDay <= $countDays) {
+        // попадает текущий день на рабочий день
+        if ($workDays % 3 == 0) {
+            // рабочий день
+            $list[] = "\033[31m$currentDay\033[0m";
+            $workDays++;
+        } else {
+            // выходной день
+            $list[] = "$currentDay ";
+            // если это выходной день, то пропускаем 2 дня
+            if ($workDays % 3 == 2) {
+                $currentDay += 2;
+                $workDays++;
+            } else {
+                $workDays++;
+            }
+        }
+        $currentDay++;
+    }
+
+    // результат
+    echo "$monthName "."$year"."\n";
+    foreach ($list as $day) {
+        echo $day . "\n";
+    }
+}
+
+// год и месяц который расчитываю
+$year = 2025;
+$month = 9;
+
+generateList($year, $month); 
+?>
